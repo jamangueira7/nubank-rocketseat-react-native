ce3761ea44d61652a92ad1dbece6f1500e90b1fa
João Mangueira <jamjs7@gmail.com>
2021-01-30 14:20:37 -0300

    Main já com animação

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import { Animated } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+
+
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
 import Menu from '~/components/Menu';
@@ -18,26 +22,82 @@ import {
 } from '~/pages/Main/styles';
 
 export default function Main() {
+  let offset = 0;
+  const translateY = new Animated.Value(0);
+
+  const animatedEvent = new Animated.event(
+    [
+      {
+        nativeEvent: {
+          translateY: translateY,
+        },
+      },
+    ],
+    { useNativeDriver: true },
+  );
+
+  function onHandlerStateChanged(event) {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      let opened = false;
+      const { translationY } = event.nativeEvent;
+
+      offset += translationY;
+
+      if (translationY >= 100) {
+        opened = true;
+      } else {
+        translateY.setValue(offset);
+        translateY.setOffset(0);
+        offset = 0;
+      }
+
+      Animated.timing(translateY, {
+        toValue: opened ? 380 : 0,
+        duration: 200,
+        useNativeDriver: true,
+      }).start(() => {
+        offset = opened ? 380 : 0;
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+      });
+    }
+  }
   return (
     <Container>
       <Header />
       <Content>
-        <Menu />
-        <Card>
-          <CardHeader>
-            <Icon name="attach-money" size={28} color="#666" />
-            <Icon name="visibility-off" size={28} color="#666" />
-          </CardHeader>
-          <CardConent>
-            <Title>Saldo disponível</Title>
-            <Description>R$7.655,05</Description>
-          </CardConent>
-          <CardFooter>
-            <Annotation>
-              Tranfêrencia de R$ 35,00 recebida de Jose Silva hoje as 06:00h
-            </Annotation>
-          </CardFooter>
-        </Card>
+        <Menu translateY={translateY} />
+
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChanged}
+        >
+          <Card style={{
+            transform: [{
+              translateY: translateY.interpolate({
+                inputRange: [-350, 0, 380],
+                outputRange: [-50, 0, 380],
+                extrapolate: 'clamp',
+              }),
+            }],
+          }}
+          >
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
+            <CardConent>
+              <Title>Saldo disponível</Title>
+              <Description>R$7.655,05</Description>
+            </CardConent>
+            <CardFooter>
+              <Annotation>
+                Tranfêrencia de R$ 35,00 recebida de Jose Silva hoje as 06:00h
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
+
       </Content>
       <Tabs />
     </Container>
