22ab236c67269e4600e30c0875b63b9435bcfdd6
João Mangueira <jamjs7@gmail.com>
2021-01-30 13:52:36 -0300

    Menu pronto

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import QRCode from 'react-native-qrcode-svg';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import {
   Container,
   Code,
+  Nav,
+  NavItem,
+  NavText,
+  SingOutButton,
+  SingOutButtonText,
 } from '~/components/Menu/styles';
 
 
@@ -18,6 +24,27 @@ export default function Menu() {
           color="#8B10AE"
         />
       </Code>
+      <Nav>
+        <NavItem>
+          <Icon name="help-outline" size={20} color="#FFF" />
+          <NavText>Me ajuda</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="person-outline" size={20} color="#FFF" />
+          <NavText>Perfil</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="credit-card" size={20} color="#FFF" />
+          <NavText>Configurar cartão</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="smartphone" size={20} color="#FFF" />
+          <NavText>Configurações do app</NavText>
+        </NavItem>
+      </Nav>
+      <SingOutButton onPress={() => {}}>
+        <SingOutButtonText>SAIR DO APP</SingOutButtonText>
+      </SingOutButton>
     </Container>
   );
 }
